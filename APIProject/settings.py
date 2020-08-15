0f20466049085dfd6a9c3423d7991a25cea7c781
stalker55 <ferranfer@gmail.com>
2020-08-15 11:06:19 +0200

    Convert sqlite to ProstgreSql

--- APIProject/settings.py
+++ APIProject/settings.py
@@ -23,16 +23,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'rop$lqp1y6$g+0u24*1vc#2nt!&+8f@e831jst9nep=#8e^_e2'
+SECRET_KEY = 'rop$lqp1y6$g+0u24*1vc#2nt!&+8f@e831jst9nep=#8e^_e2'
 
 
-SECRET_KEY = config('SECRET_KEY')
+#SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+#DEBUG = config('DEBUG', default=False, cast=bool)
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
@@ -83,16 +83,20 @@ WSGI_APPLICATION = 'APIProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'machinelearning1',
+        'USER': 'postgres',
+        'PASSWORD': 'ecnlsnd',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
+#DATABASES = {
+#    'default': dj_database_url.config(
+#        default=config('DATABASE_URL')
+#    )
+#}
 
 
 # Password validation
